7d2ba293fbe8f89dd26dbffc38c5be2f28c0a867
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-11-13 18:30:02 +0800

    feat: added a case in scenario-1 for triangles-test.

--- triangles-test/scenario-1.cpp
+++ triangles-test/scenario-1.cpp
@@ -1,2 +1,21 @@
 #include <catch2/catch_amalgamated.hpp>
+#include "helpers.hpp"
+#include "../draw2d/surface.hpp"
+#include "../draw2d/draw.hpp"
 
+TEST_CASE("Partially Off-Screen Triangle", "[clip][special]")
+{
+    Surface surface(320, 240);
+    surface.clear();
+
+    draw_triangle_interp(surface,
+                         { -50.f, 50.f }, { 150.f, 50.f }, { 50.f, 200.f },
+                         { 0.5f, 0.3f, 0.7f },
+                         { 0.5f, 0.3f, 0.7f },
+                         { 0.5f, 0.3f, 0.7f });
+
+    auto const col = find_most_red_pixel(surface);
+    REQUIRE(col.r > 0);
+    REQUIRE(col.g > 0);
+    REQUIRE(col.b > 0);
+}
